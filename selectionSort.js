c6f16187ff68a8c3456a30f08bbb3df38d723938
Ricardo Guerrero <richi_gro@hotmail.com>
2021-01-05 17:59:24 -0600

    Lastest update, still incomplete

--- selectionSort.js
+++ selectionSort.js
@@ -2,29 +2,8 @@
 // also this algorithm is not very performant at scale
 // O(n^2) Time and O(1) Space 
 const selectionSort = function(array) {
-  let smallest;
-  let indexOfSmallest;
-  let currentIdx = 0;
-
- // we are running the loop ince for every element in the array
-  while(){
-    for(let j = 0; j < array.length; j++){
-        smallest = array[j];
-        indexOfSmallest = j;
-      //find the smallest number in the list
-      if(array[j] < smallest){
-        //change the value of the smallest number to equal the new smallest
-        smallest = array[j];
-        //keep track of the index of the smallest element
-        indexOfSmallest = j;
-      }
-    }
-    // we need to swap the smallest element on the list for the next item on the sub list
-    swap(array, i, indexOfSmallest);
-  }
- 
-  
-  return array;
+ //store first element as the samallest value you've seen so far
+ let smallest = ;
 };
 
 const swap = function(array, i, j){
