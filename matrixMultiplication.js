27c89318792f6a25155388829e990dd352a04830
ricardo guerrero <richi_gro@hotmail.com>
2021-01-23 09:23:03 -0600

    added boilerplate for matrixMultiplication

---
+++ matrixMultiplication.js
@@ -0,0 +1,19 @@
+
+function matrixMultiplication(x, y) {
+  var z = [];
+
+};
+
+//both matricies much match 
+//if matrix A . B = C, where n x m & e x h, where m === e for the multiplication to be true
+var A = [
+  [1,2], 
+  [3,4]
+];
+
+var B = [
+  [5,6],
+  [7,8]
+];
+var row = col = 0;
+console.log(A[row][col + 1]);
