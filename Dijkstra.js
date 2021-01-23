5e5bff66900d08395f6c0a8626942362285492bd
ricardo guerrero <richi_gro@hotmail.com>
2021-01-22 23:06:49 -0600

    testing cloned repo

--- Dijkstra.js
+++ Dijkstra.js
@@ -1,3 +1,4 @@
 // this is to traverse a weighted graph that do not contain cycles 
 // 1>) find the cheapest next node
 // 2) re-caculate the weights on each edge
+//yvyiggggygyigyigiygiygiygiygyigigigig
