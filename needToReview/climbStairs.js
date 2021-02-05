e3f63b9ff7ee574bb5ce4c9ce7ba644678bedc7d
Ricardo Guerrero <richi_gro@hotmail.com>
2021-02-05 14:05:48 -0600

    completed solution with dynamic programming (dp)

--- needToReview/climbStairs.js
+++ needToReview/climbStairs.js
@@ -24,19 +24,45 @@
 // runtime O(n) spcae O(n)
 // this solution uses memoization
 
-var climbStairs = function(n) {
-  var memo = new Array(n + 1);
-  //base cases
-  memo[0] = 1;
-  memo[1] = 1;
-  console.log(memo);
+// var climbStairs = function(n) {
+//   var memo = new Array(n + 1);
+//   //base cases
+//   memo[0] = 1;
+//   memo[1] = 1;
+//   console.log(memo);
 
-  //fill the rest of the array
-  for(let i = 2; i < memo.length; i++){
-      memo[i] = memo[i - 2] + memo[i - 1];
-  }
-  console.log(memo);
-  return memo[memo.length - 1];
+//   //fill the rest of the array
+//   for(let i = 2; i < memo.length; i++){
+//       memo[i] = memo[i - 2] + memo[i - 1];
+//   }
+//   console.log(memo);
+//   return memo[memo.length - 1];
+// };
+
+//completed with basic dynamic programming
+var climbStairs = function(n) {
+  // var obj = {};
+    
+   var countSteps = function(steps, obj){
+       //base case
+       if(steps < 0){
+          return 0;
+       } 
+
+       if(steps === 0){
+           return 1;
+       }
+       //if already calculated return the previous result 
+       if(obj[steps]){
+          return obj[steps];
+        }
+       
+        // calculate a never before seen case
+        obj[steps] = countSteps(steps - 1, obj) + countSteps(n - 2, obj);
+        return obj[steps];
+   };
+    
+    return countSteps(n, {});
 };
 
-console.log(climbStairs(45));
+console.log(climbStairs(2));
