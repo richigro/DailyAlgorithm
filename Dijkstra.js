4fb241d89d406935933d8d0b538b2acf67e392bf
Ricardo Guerrero <richi_gro@hotmail.com>
2021-02-06 21:05:24 -0600

    Update Dijkstra.js

--- Dijkstra.js
+++ Dijkstra.js
@@ -1,4 +1,5 @@
 // this is to traverse a weighted graph that do not contain cycles 
 // 1>) find the cheapest next node
 // 2) re-caculate the weights on each edge
-//yvyiggggygyigyigiygiygiygiygyigigigig
+//yvyiggggygyigyigiygiygiygiygyigigigi
+// priority queue
